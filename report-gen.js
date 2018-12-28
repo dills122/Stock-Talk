10d86049a67a9b88f4f3e675061800755cae795b
Steele, Dylan <dylansteele@nexsystech.com>
2018-12-28 13:30:38 -0500

    Cleaned up report generation and better formatting

--- report-gen.js
+++ report-gen.js
@@ -1,37 +1,47 @@
 const prettyJson = require('prettyjson');
 const numeral = require('numeral');
 
-function CompileStockList(values, type) {
-    if (!values) {
-        return "Error";
-    }
-    var compiledList = [];
-    if (type === 's') {
-        values.forEach(element => {
-            //Squashed report
-            compiledList.push(generateSquashedReport(element));
-        });
-    }
+const options = {
+    noColor: false
+};
 
-    return type === 'f' ? values : compiledList;
+function GenerateStockReport(stockInfo, type) {
 
-    function generateSquashedReport(value) {
+    console.log(prettyJson.render(type === 'f' ? fullReport(stockInfo) : squashedReport(stockInfo), options));
+
+    function fullReport(stockInfo) {
         return {
-            'high': numeral(value.high).format('$0,0.00'),
-            'low': numeral(value.low).format('$0,0.00'),
-            'average': numeral(value.average).format('$0,0.00'),
-            'volume': value.volume,
+            'date': FormatDate(stockInfo.date),
+            'high': numeral(stockInfo.high).format('$0,0.00'),
+            'low': numeral(stockInfo.low).format('$0,0.00'),
+            'average': numeral(stockInfo.average).format('$0,0.00'),
+            'volume': stockInfo.volume,
+            'marketHigh': stockInfo.marketHigh,
+            'marketLow': stockInfo.marketLow,
+            'marketAverage': stockInfo.marketAverage,
+            'marketVolume': stockInfo.marketVolume,
+            'open': stockInfo.open,
+            'close': stockInfo.close,
+            'marketOpen': stockInfo.marketOpen,
+            'marketClose': stockInfo.marketClose
+        };
+    }
+
+    function squashedReport(stockInfo) {
+        return {
+            'date': FormatDate(stockInfo.date),
+            'high': numeral(stockInfo.high).format('$0,0.00'),
+            'low': numeral(stockInfo.low).format('$0,0.00'),
+            'average': numeral(stockInfo.average).format('$0,0.00'),
+            'volume': stockInfo.volume,
         };
     }
 }
 
-function GenerateStockReport(values, type) {
-    values = CompileStockList(values, type);
-    values.forEach(element => {
-        console.log(prettyJson.render(element, {
-            noColor: false
-        }));
-    });
+function FormatDate(date) {
+    if(date) {
+        return date.substring(4,6) + '-' + date.substring(6) + '-' +  date.substring(0,4);
+    }
 }
 
 module.exports = {
